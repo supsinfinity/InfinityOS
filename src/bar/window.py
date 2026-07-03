85a69aba4a621c4a73c0616e399aef74fea69dfe
Aarav Gupta <g.aarav28@icloud.com>
2026-07-03 15:20:33 -0500

    feat(clock): add live updating clock

--- src/bar/window.py
+++ src/bar/window.py
@@ -1,8 +1,8 @@
 import gi
+from datetime import datetime
 
 gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk
-
+from gi.repository import Gtk, GLib
 
 class InfinityWindow(Gtk.ApplicationWindow):
     def __init__(self, application):
@@ -26,12 +26,22 @@ class InfinityWindow(Gtk.ApplicationWindow):
         music = Gtk.Label(label="🎵 No music playing")
 
         # Right section
-        clock = Gtk.Label(label="🕒 00:00")
+        self.clock = Gtk.Label(label="🕒 --:--")
 
         # Add widgets to the CenterBox
         center_box.set_start_widget(infinity)
         center_box.set_center_widget(music)
-        center_box.set_end_widget(clock)
+        center_box.set_end_widget(self.clock)
 
         # Make the CenterBox the window contents
         self.set_child(center_box)
+        self.start_clock()
+
+    def update_clock(self):
+        now = datetime.now()
+        self.clock.set_label(now.strftime("🕒 %I:%M:%S %p"))
+        return True
+
+    def start_clock(self):
+        self.update_clock()          # Update immediately
+        GLib.timeout_add_seconds(1, self.update_clock)
